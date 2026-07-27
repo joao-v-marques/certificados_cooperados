df38f8b4839d82f533d08606da93c57288161702
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 16:04:36 -0300

    first commit

---
+++ src/test/java/com/joao_v_marques/certificados_cooperados/CertificadosCooperadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao_v_marques.certificados_cooperados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificadosCooperadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
