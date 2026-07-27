569790177cedd4d7dfac9e538a874e4b5514ce9b
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 17:04:37 -0300

    feat: adicionado arquivo de SecurityConfig para configuração inicial do Spring Security, visando impedir que ele não deixe rodar a aplicação sem antes realizar a lógica de autenticação de usuários

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.joao_v_marques.certificados_cooperados.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // TEMPORARIO: libera tudo enquanto ainda não existe um login de usuário !!!
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+}
