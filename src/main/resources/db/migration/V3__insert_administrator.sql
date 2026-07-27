19ebf7066ea943788ac629b85f3d531559bfd33b
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 17:05:39 -0300

    feat: adicionado migration para cadastrar um usuário administrador como padrão do sistema

---
+++ src/main/resources/db/migration/V3__insert_administrator.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (username, name, password_hash, email, role_id)
+VALUES ('admin', 'Administrador', '$2a$10$Jmcp3XcpzZ7T9lvhiW5OVOaZRAEMtIX/mAgA24gkGltdiT0cFxy4O', 'joaovictor@unimedssp.coop.br', 1);
