6bbe47d9782e870b0377b69d2ff9a0377827dcb9
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 20:52:16 -0300

    feat: adicionado nova entidade de Role nas entity

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/entity/Role.java
@@ -0,0 +1,20 @@
+package com.joao_v_marques.certificados_cooperados.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, unique = true, length = 100)
+    private String name;
+}
