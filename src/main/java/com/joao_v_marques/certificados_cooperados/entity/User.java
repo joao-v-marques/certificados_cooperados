114384ffc6b63124da3ed3ae7de2ced600014dcd
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 21:25:58 -0300

    feat: adicionado nova entidade de User

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/entity/User.java
@@ -0,0 +1,42 @@
+package com.joao_v_marques.certificados_cooperados.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, unique = true, length = 155)
+    private String username;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "password_hash", nullable = false, unique = false, length = 255)
+    private String passwordHash;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "created_at", nullable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id", nullable = false)
+    private Role role;
+
+    @Column(name = "is_active", nullable = false)
+    private boolean isActive = true;
+}
