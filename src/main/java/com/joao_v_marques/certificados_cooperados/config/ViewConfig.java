c2d13bad193db02060b86e4191ef27f2ee9ba08d
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-28 09:54:37 -0300

    feat: adicionado arquivo de configurações para renderizar páginas html utilizando Thymeleaf

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/config/ViewConfig.java
@@ -0,0 +1,14 @@
+package com.joao_v_marques.certificados_cooperados.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ViewConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
