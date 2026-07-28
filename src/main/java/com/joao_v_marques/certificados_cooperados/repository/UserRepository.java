7ddc4722130d9100a1c11c8fcc70179dc6ce3686
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-27 22:22:51 -0300

    feat: adicionado repository com JpaRepository para User

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.joao_v_marques.certificados_cooperados.repository;
+
+import com.joao_v_marques.certificados_cooperados.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
