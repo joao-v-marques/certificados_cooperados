440649dae249074970be75cbc1d33a62e4868963
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-29 17:22:01 -0300

    feat: criado arquivo de controller para o course, definido o endpoint padrão com versionamento e já criado endpoint para POST de cursos atomico

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/controller/CourseController.java
@@ -0,0 +1,34 @@
+package com.joao_v_marques.certificados_cooperados.controller;
+
+import com.joao_v_marques.certificados_cooperados.dto.CourseRequest;
+import com.joao_v_marques.certificados_cooperados.dto.CourseResponse;
+import com.joao_v_marques.certificados_cooperados.service.CourseService;
+import jakarta.validation.Valid;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/courses")
+public class CourseController {
+
+    private final CourseService courseService;
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<CourseResponse> create(@Valid @ModelAttribute CourseRequest request, @RequestPart("certificate") MultipartFile certificate) {
+        // TODO: trocar pelo id do Authentication quando o login existir
+        Integer currentUserId = 1;
+
+        CourseResponse created = courseService.create(request, certificate, currentUserId);
+
+        URI location = URI.create("/api/v1/courses/" + created.id());
+
+        return ResponseEntity.created(location).body(created);
+    }
+}
