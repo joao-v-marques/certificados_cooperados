2d0b9017768639e3e7938eac338964e055879319
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-29 17:23:42 -0300

    feat: adicionado dto para a Response de um curso, é alimentado com basicamente todas as informações que não são chaves para retornar ao realizar um POST ou ao realizar uma consulta geral findAll

---
+++ src/main/java/com/joao_v_marques/certificados_cooperados/dto/CourseResponse.java
@@ -0,0 +1,16 @@
+package com.joao_v_marques.certificados_cooperados.dto;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+
+// Dto de retorno, será utilizado para a saída de POST e caso seja necessário listar todos os cursos
+public record CourseResponse(
+        Integer id,
+        String title,
+        int totalMinutes,
+        LocalDate completionDate,
+        String observations,
+        OffsetDateTime createdAt,
+        String cooperativeMemberName,
+        String insertedByName
+) {}
