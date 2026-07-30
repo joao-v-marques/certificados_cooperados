a39f6c214829ba1327d23b800c463d311194bd95
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-30 00:17:18 -0300

    fix: adicionado query fetch no repository para login

--- src/main/java/com/joao_v_marques/certificados_cooperados/repository/UserRepository.java
+++ src/main/java/com/joao_v_marques/certificados_cooperados/repository/UserRepository.java
@@ -2,10 +2,13 @@ package com.joao_v_marques.certificados_cooperados.repository;
 
 import com.joao_v_marques.certificados_cooperados.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u JOIN FETCH u.role WHERE u.username = :username")
+    Optional<User> findByUsername(@Param("username") String username);
 }
